e3fdffbe5baca849c3c05a890ac6bb2f62c49055
Benjamin Poon <benjamintpoon@gmail.com>
2018-02-28 10:32:13 -0600

    starting over from re-recorded course 2/28

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,1 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './css/style.css';
-
-import StorePicker from './components/StorePicker';
-
-render(<StorePicker/>, document.querySelector('#main'));
+// let's go!
