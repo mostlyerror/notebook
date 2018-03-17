476922514e984736a2cab8f0c86a4f00cf91f9b2
Benjamin Poon <benjamintpoon@gmail.com>
2018-03-17 00:28:14 -0500

    Popular component with clickhandler, illustrating how to bind event handlers

---
+++ react-fundamentals/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import logo from '../logo.svg';
+import '../App.css';
+import Popular from './Popular.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <Popular />
+      </div>
+    );
+  }
+}
+
+// module.exports = App;
+export default App;
