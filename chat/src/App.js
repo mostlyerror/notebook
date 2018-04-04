795f07327261b77df4b48d42a4311f0b19d7e6e6
Benjamin Poon <benjamintpoon@gmail.com>
2018-04-03 22:57:02 -0500

    create react redux redux-saga chat application with styles

---
+++ chat/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div id='container'>
+        <aside id='sidebar'>Users</aside>
+        <section id='main'>
+          <section id='messages-list'>Messages list</section>
+          <section id='new-message'>New message</section>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
